360df241a3a408806300e212b318aa93976afd57
ivan-pl <pleskaiv@gmail.com>
2022-05-16 12:19:17 +0300

    build: setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
@@ -11,5 +12,13 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'max-len': ['error', { ignoreComments: true }],
+    'import/prefer-default-export': 'off',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
+  plugins: ['jest'],
 };
