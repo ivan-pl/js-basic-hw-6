073fe5e2427b7c579f19b1292630f687196fd8fc
ivan-pl <pleskaiv@gmail.com>
2022-05-18 17:00:45 +0300

    refactor: edit description for task1 tests

--- src/hw2.test.js
+++ src/hw2.test.js
@@ -3,22 +3,22 @@ import { task1 } from "./hw2";
 describe("task1", () => {
   const spy = jest.spyOn(console, "log");
 
-  it("returns 5 for task1(1, 5)", () => {
+  it("calls console.log with 5 for task1(1, 5)", () => {
     task1(1, 5);
     expect(spy).toBeCalledWith(5);
   });
 
-  it("returns 5 for task1(5, 1)", () => {
+  it("calls console.log with 5 for task1(5, 1)", () => {
     task1(5, 1);
     expect(spy).toBeCalledWith(5);
   });
 
-  it("returns 0 for task1(0, -5)", () => {
+  it("calls console.log with 0 for task1(0, -5)", () => {
     task1(0, -5);
     expect(spy).toBeCalledWith(0);
   });
 
-  it("returns -3 for task1(-24, -3)", () => {
+  it("calls console.log with -3 for task1(-24, -3)", () => {
     task1(-24, -3);
     expect(spy).toBeCalledWith(-3);
   });
