81f6fecbb157b94b211a6491e6b779d0f037ab5b
ivan-pl <pleskaiv@gmail.com>
2022-05-19 14:46:24 +0300

    fix: replace jest.fn with jest.spyOn for console.log

--- src/hw1.test.js
+++ src/hw1.test.js
@@ -1,20 +1,25 @@
 import { task1, task2, task3 } from "./hw1";
 
 describe("task1", () => {
+  beforeAll(() => {
+    jest.spyOn(console, "log");
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
   it('calls console.log with "mul = 12; sum = 8" for "task1(2, 6)"', () => {
-    console.log = jest.fn();
     task1(2, 6);
     expect(console.log).toHaveBeenCalledWith("mul = 12; sum = 8");
   });
 
   it('calls console.log with "mul = 12; sum = 8" for "task1(6, 2)"', () => {
-    console.log = jest.fn();
     task1(6, 2);
     expect(console.log).toHaveBeenCalledWith("mul = 12; sum = 8");
   });
 
   it('calls console.log with "mul = 1000; sum = 110" for "task1(100, 10)"', () => {
-    console.log = jest.fn();
     task1(100, 10);
     expect(console.log).toHaveBeenCalledWith("mul = 1000; sum = 110");
   });
