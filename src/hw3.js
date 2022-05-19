6ccf4ad41df570ed4713b9e1ebc8738d59a5400f
ivan-pl <pleskaiv@gmail.com>
2022-05-19 12:37:35 +0300

    feat: solve task2

--- src/hw3.js
+++ src/hw3.js
@@ -8,3 +8,16 @@ export function task1() {
   }
   console.log(sum);
 }
+
+/**
+ * Вывести в консоль таблицу умножения на 7.
+ * 7 x 1 = 7
+ * 7 x 2 = 14
+ * …
+ * 7 x 9 = 63
+ */
+export function task2() {
+  for (let i = 1; i < 10; i++) {
+    console.log(`7 x ${i} = ${7 * i}`);
+  }
+}
--- src/hw3.test.js
+++ src/hw3.test.js
@@ -1,4 +1,4 @@
-import { task1 } from "./hw3";
+import { task1, task2 } from "./hw3";
 
 describe("task1", () => {
   const sum = (50 * (50 + 99)) / 2;
@@ -6,5 +6,42 @@ describe("task1", () => {
     console.log = jest.fn();
     task1();
     expect(console.log).toBeCalledWith(sum);
+  });
+});
+
+describe("task2", () => {
+  console.log = jest.fn();
+
+  it("calls console.log 9 times", () => {
+    task2();
+    expect(console.log.mock.calls).toHaveLength(9);
+  });
+
+  it("calls console.log with 1 argument", () => {
+    task2();
+    expect(console.log.mock.calls.every((item) => item.length === 1)).toBe(
+      true
+    );
+  });
+
+  it("first call console.log is 7 x 1 = 7", () => {
+    task2();
+    expect(console.log.mock.calls[0][0]).toBe("7 x 1 = 7");
+  });
+
+  it("last call console.log is 7 x 9 = 63", () => {
+    task2();
+    expect(console.log.mock.calls[console.log.mock.calls.length - 1][0]).toBe(
+      "7 x 9 = 63"
+    );
+  });
+
+  it("corresponds to template", () => {
+    const template = Array.from(
+      { length: 9 },
+      (item, ind) => `7 x ${ind + 1} = ${(ind + 1) * 7}`
+    );
+    task2();
+    expect(console.log.mock.calls.map((item) => item[0])).toEqual(template);
   });
 });
