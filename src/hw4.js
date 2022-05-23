4fae54ad85411a61a1ed0a8ab0acf5a63e216a2e
ivan-pl <pleskaiv@gmail.com>
2022-05-23 10:40:30 +0300

    feat: add task2

--- src/hw4.js
+++ src/hw4.js
@@ -9,3 +9,15 @@ export function task1() {
   user.age = prompt("Enter an age: ", 18);
   return user;
 }
+
+/**
+ * Создайте объект user, содержащий поле name со
+ * значением ‘John’.
+ * 2.Создать копию объекта user с именем admin.
+ * Добавить новому объекту поле role со значением
+ * ‘admin’.
+ */
+export function task2(obj) {
+  const admin = {role: "admin", ...obj};
+  return admin;
+}
--- src/hw4.test.js
+++ src/hw4.test.js
@@ -1,4 +1,4 @@
-import { task1 } from "./hw4";
+import { task1, task2 } from "./hw4";
 
 describe("task1", () => {
   beforeEach(() => {
@@ -9,7 +9,7 @@ describe("task1", () => {
     jest.clearAllMocks();
   });
 
-  it("returns Object", () => {
+  it("returns object", () => {
     expect(typeof task1()).toBe("object");
   });
 
@@ -20,5 +20,29 @@ describe("task1", () => {
   it('property "age" contains 5', () => {
     jest.spyOn(globalThis, "prompt").mockReturnValue(5);
     expect(task1().age).toBe(5);
+  });
+});
+
+describe("task2", () => {
+  let user = { name: "John" };
+
+  afterEach(() => {
+    user = { name: "John" };
+  });
+
+  it("returns object", () => {
+    expect(typeof task2(user)).toBe("object");
+  });
+
+  it("is another object", () => {
+    expect(task2(user)).not.toBe(user);
+  });
+
+  it('contains property name = "John"', () => {
+    expect(task2(user).name).toBe("John");
+  });
+
+  it('property "role" contains "admin"', () => {
+    expect(task2(user).role).toBe("admin");
   });
 });
