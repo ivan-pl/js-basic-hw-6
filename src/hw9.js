23a5183cf4f99653aab4a5e0284589c81fb15ffa
ivan-pl <pleskaiv@gmail.com>
2022-05-24 17:43:40 +0300

    feat: add task2

--- src/hw9.js
+++ src/hw9.js
@@ -12,5 +12,14 @@
 export function isRightTriangle(side1, side2, side3) {
   const [a, b, c] = [side1, side2, side3].sort((item1, item2) => item1 - item2);
   const square = (a ** 2 + b ** 2).toFixed(2);
-  return square === (c**2).toFixed(2);
+
+  return square === (c ** 2).toFixed(2);
+}
+
+export function task2() {
+  const r = +prompt("Enter a number:", 1);
+  const c = 2 * Math.PI * r;
+  const s = Math.PI * r**2;
+
+  console.log(`C = ${c.toFixed(3)}; S = ${s.toFixed(3)}`);
 }
--- src/hw9.test.js
+++ src/hw9.test.js
@@ -1,4 +1,4 @@
-import { isRightTriangle } from "./hw9";
+import { isRightTriangle, task2 } from "./hw9";
 
 describe("task1", () => {
   it("returns true for isRightTriangle(3, 4, 5)", () => {
@@ -15,5 +15,23 @@ describe("task1", () => {
 
   it("returns true for isRightTriangle(1, 2.236, 2)", () => {
     expect(isRightTriangle(1, 2.236, 2)).toBe(true);
+  });
+});
+
+describe("task2", () => {
+  beforeAll(() => {
+    jest.spyOn(console, "log");
+  });
+
+  it('calls console.log with "C = 12.566; S = 12.566" for R = 2', () => {
+    jest.spyOn(globalThis, "prompt").mockReturnValue(2);
+    task2();
+    expect(console.log).toBeCalledWith("C = 12.566; S = 12.566");
+  });
+
+  it('calls console.log with "C = 94.248; S = 706.858" for R = 15', () => {
+    jest.spyOn(globalThis, "prompt").mockReturnValue(15);
+    task2();
+    expect(console.log).toBeCalledWith("C = 94.248; S = 706.858");
   });
 });
