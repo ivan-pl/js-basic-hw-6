283184946efa939274787afd31092b978136c4bf
ivan-pl <pleskaiv@gmail.com>
2022-05-25 11:52:18 +0300

    feat: add task3

--- src/hw9.js
+++ src/hw9.js
@@ -19,7 +19,25 @@ export function isRightTriangle(side1, side2, side3) {
 export function task2() {
   const r = +prompt("Enter a number:", 1);
   const c = 2 * Math.PI * r;
-  const s = Math.PI * r**2;
+  const s = Math.PI * r ** 2;
 
   console.log(`C = ${c.toFixed(3)}; S = ${s.toFixed(3)}`);
+}
+
+export function task3() {
+  const [a, b, c] = prompt("Enter a, b and c with a space:", "1 2 3")
+    .split(" ")
+    .map((x) => +x);
+  const d = b ** 2 - 4 * a * c;
+
+  if (d > 0) {
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
+    alert(`${x1}, ${x2}`);
+  } else if (d < 0) {
+    alert("No roots");
+  } else {
+    const x = -b / (2 * a);
+    alert(`${x}`);
+  }
 }
--- src/hw9.test.js
+++ src/hw9.test.js
@@ -1,4 +1,4 @@
-import { isRightTriangle, task2 } from "./hw9";
+import { isRightTriangle, task2, task3 } from "./hw9";
 
 describe("task1", () => {
   it("returns true for isRightTriangle(3, 4, 5)", () => {
@@ -33,5 +33,59 @@ describe("task2", () => {
     jest.spyOn(globalThis, "prompt").mockReturnValue(15);
     task2();
     expect(console.log).toBeCalledWith("C = 94.248; S = 706.858");
+  });
+});
+
+describe("task3", () => {
+  beforeAll(() => {
+    jest.spyOn(globalThis, "alert").mockReturnThis();
+  });
+
+  it("calls alert with 2, -2 for 2 0 -8", () => {
+    jest.spyOn(globalThis, "prompt").mockReturnValueOnce("2 0 -8");
+    task3();
+
+    expect(alert.mock.calls).toHaveLength(1);
+    const results = alert.mock.calls[0][0];
+    expect(typeof results).toBe("string");
+
+    const resultsArray = results.split(", ");
+    expect(resultsArray).toHaveLength(2);
+    expect(resultsArray).toEqual(expect.arrayContaining(["2", "-2"]));
+  });
+
+  it("calls alert with 0, -3 for 2 6 0", () => {
+    jest.spyOn(globalThis, "prompt").mockReturnValueOnce("2 6 0");
+    task3();
+
+    expect(alert.mock.calls).toHaveLength(1);
+    const results = alert.mock.calls[0][0];
+    expect(typeof results).toBe("string");
+
+    const resultsArray = results.split(", ");
+    expect(resultsArray).toHaveLength(2);
+    expect(resultsArray).toEqual(expect.arrayContaining(["0", "-3"]));
+  });
+
+  it("calls alert with 1, -3 for 1 2 -3", () => {
+    jest.spyOn(globalThis, "prompt").mockReturnValueOnce("1 2 -3");
+    task3();
+
+    expect(alert.mock.calls).toHaveLength(1);
+    const results = alert.mock.calls[0][0];
+    expect(typeof results).toBe("string");
+
+    const resultsArray = results.split(", ");
+    expect(resultsArray).toHaveLength(2);
+    expect(resultsArray).toEqual(expect.arrayContaining(["1", "-3"]));
+  });
+
+  it("calls alert with 3 for 1 -6 9", () => {
+    jest.spyOn(globalThis, "prompt").mockReturnValue("1 -6 9");
+    task3();
+
+    expect(alert.mock.calls).toHaveLength(1);
+    const result = alert.mock.calls[0][0];
+    expect(result).toBe("3");
   });
 });
